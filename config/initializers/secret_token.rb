df4d0264ac6dcc43162507200c04238d296c191c
steveyang94 <yangshiqi1q2w3e@gmail.com>
2014-10-26 14:23:56 +0800

    checkbox, sort by attributes, etc

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DocBox::Application.config.secret_token = '88e0ce0cee8b68b277f7138b168de769642dcfc0fd5199ad8f0acc18b5c4cbf6cf490455662b8ce5852a4beda34d9842bc91c59f633b4916529eebb4b8369e0c'
